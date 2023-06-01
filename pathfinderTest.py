f1177bc1af00dd48cf2b7b24da8d56b303f27210
Valdemar Nielsen <v.nielsen@live.dk>
2023-06-01 15:46:29 +0200

    Updates to finding algorithm and move-decider.

--- pathfinderTest.py
+++ pathfinderTest.py
@@ -1,5 +1,8 @@
 import math
 
+import MoverFinder
+import moveOptions
+
 # import main
 
 
@@ -28,6 +31,11 @@ def calculate_angle(ball_x, ball_y, robot_x, robot_y):
     return angle
 
 
+def find_goal_distance(goal_x, goal_y, robot_x, robot_y):
+    goal_distance = math.sqrt((goal_x - robot_x) ** 2 + (goal_y - robot_y) ** 2)
+    return goal_distance
+
+
 def make_move(frame):
     ball_locations = [[2, 4], [2, 3], [8, 8], [8, 3], [10, 10], [20, 20], [1, 9], [16, 14]]  # Example of table balls
     orange_ball = [0, 0]  # the orange ball
@@ -37,9 +45,33 @@ def make_move(frame):
     closest_ball_location, distanceToBall = find_nearest_ball(robot_location[0], robot_location[1], ball_locations)
     angle_to_turn = calculate_angle(closest_ball_location[0], closest_ball_location[1], robot_location[0],
                                     robot_location[1])
-    return distanceToBall, angle_to_turn
+    angle_to_goal = calculate_angle(goal_x, goal_y, robot_location[0], robot_location[1])
+    goalDistance = find_goal_distance(goal_x, goal_y, robot_location[0], robot_location[1])
+
+    if angle_to_goal > 0 & len(ball_locations) == 5 | 3 | 2 | 1:
+        return MoverFinder.MoveClass(moveOptions.RIGHT, 300, angle_to_goal), MoverFinder.MoveClass(moveOptions.FORWARD,
+                                                                                                   500, goalDistance)
+    if angle_to_goal < 0 & len(ball_locations) == 5 | 3 | 2 | 1:
+        return MoverFinder.MoveClass(moveOptions.LEFT, 300, angle_to_goal), MoverFinder.MoveClass(moveOptions.FORWARD,
+                                                                                                  500, goalDistance)
+    if angle_to_goal == 0 & len(ball_locations) == 5 | 3 | 2 | 1:
+        return MoverFinder.MoveClass(moveOptions.FORWARD, 500, goalDistance)
+
+    if angle_to_turn > 0:
+        return MoverFinder.MoveClass(moveOptions.RIGHT, 300, angle_to_turn), MoverFinder.MoveClass(moveOptions.FORWARD,
+                                                                                                   500, distanceToBall)
+    if angle_to_turn == 0:
+        return MoverFinder.MoveClass(moveOptions.FORWARD, 500, distanceToBall)
+
+    if angle_to_turn < 0:
+        return MoverFinder.MoveClass(moveOptions.LEFT, 300, angle_to_turn), MoverFinder.MoveClass(moveOptions.FORWARD,
+                                                                                                  500, distanceToBall)
+
+    return MoverFinder.MoveClass(moveOptions.UNSTUCK, 500, 100)
 
 
-distance, angle = make_move()
-print("Distance is: "+str(distance))
-print("Angle is: "+str(angle))
+angle, distance = make_move(None)
+print("Distance is: ")
+distance.print()
+print("Angle is: ")
+angle.print()
