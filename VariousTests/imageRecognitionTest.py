c567216331db9bdc479455f9e6f1927abf90c358
siggo <sig.goldbach@gmail.com>
2023-06-04 12:07:38 +0200

    Three new pictures to use fo obstacle checking

--- VariousTests/imageRecognitionTest.py
+++ VariousTests/imageRecognitionTest.py
@@ -2,10 +2,12 @@ import cv2 as cv
 
 import ImFromPhoto
 
-image = cv.imread('../Resources/Pictures/testRun1.jpg')
+image = cv.imread('../Resources/Pictures/CheckForObstacle3.jpg')
 
 balls, front, back = ImFromPhoto.imageRecognition(image)
 print("Result from test: ")
+print("I found: "+str(len(balls))+" Balls")
+
 
 # print(balls)
 # print(robot)
--- imageReader.py
+++ imageReader.py
@@ -10,7 +10,7 @@ while 1:
         break
     cv.imshow('Read picture', image)
     if cv.waitKey(1) & 0xFF == ord('q'):
-        cv.imwrite('Resources/Pictures/TestRun1.jpg', image)
+        cv.imwrite('Resources/Pictures/CheckForObstacle3.jpg', image)
         break
 
 videoCapture.release()
