990c7e71e0699caa05da8b407ba8e3f4092e1efc
seburu <sebynoah@gmail.com>
2023-06-05 10:38:43 +0200

    Img recog update

--- imageReader.py
+++ imageReader.py
@@ -10,7 +10,7 @@ while 1:
         break
     cv.imshow('Read picture', image)
     if cv.waitKey(1) & 0xFF == ord('q'):
-        cv.imwrite('Resources/Pictures/CheckForObstacle4.jpg', image)
+        cv.imwrite('Resources/Pictures/NewHeight3.jpg', image)
         break
 
 videoCapture.release()
