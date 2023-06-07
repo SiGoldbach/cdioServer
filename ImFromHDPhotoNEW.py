b8421087e970374c15b893a4267bc8271c3a2749
seburu <sebynoah@gmail.com>
2023-06-07 09:40:31 +0200

    Img recog additions

--- ImFromHDPhotoNEW.py
+++ ImFromHDPhotoNEW.py
@@ -9,7 +9,7 @@ def imageRecognitionHD(image):
 
     height, width = image.shape[:2]
 
-    blank = np.zeros((height, width, 3), dtype='uint8')
+    blank = np.zeros((height, width, 3), dtype=np.uint8)
 
     start = time.time()
 
@@ -57,36 +57,34 @@ def imageRecognitionHD(image):
         for pt in detected_circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
 
-            if (50 >= image[b, a][0]) & (160 <= image[b, a][1]) & (200 >= image[b, a][1]) & (160 <= image[b, a][2]):
+            if np.logical_and.reduce((50 >= image[b, a][0], 160 <= image[b, a][1], 200 >= image[b, a][1], 160 <= image[b, a][2])):
                 print("CENTER OF ORANGE BALL SHOULD BE: " + str(a) + " " + str(b))
                 cv.circle(blank, (a, b), r, (0, 150, 255), -1)
                 balls.append([a, b])
                 circle += 1
                 continue
 
-            if (220 <= image[b, a][2]) & (220 <= image[b, a][0]) & (220 <= image[b, a][1]):
+            if np.logical_and.reduce((220 <= image[b, a][2], 220 <= image[b, a][0], 220 <= image[b, a][1])):
                 cv.circle(blank, (a, b), r, (255, 255, 255), -1)
                 print("Center of this circle should be: " + str(a) + " " + str(b))
                 balls.append([a, b])
                 circle += 1
+
     back = []
     front = []
-
-
-
 
     if detected_Robot is not None:
         detected_circles = np.uint16(np.around(detected_Robot))
         for pt in detected_circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
-            if (220 >= image[b, a][0]) & (70 <= image[b, a][0]) & (200 <= image[b, a][1]) & (140 <= image[b, a][2]):
+            if np.logical_and.reduce((220 >= image[b, a][0], 70 <= image[b, a][0], 200 <= image[b, a][1], 140 <= image[b, a][2])):
                 print("CENTER OF GREEN BALL SHOULD BE: " + str(a) + " " + str(b))
                 cv.circle(blank, (a, b), r, (0, 255, 100), -1)
                 back.append(a)
                 back.append(b)
                 circle += 1
                 continue
-            if (150 <= image[b, a][0]) & (140 <= image[b, a][1]) & (255 >= image[b, a][1]) & (120 <= image[b, a][2]) & (220 >= image[b, a][2]):
+            if np.logical_and.reduce((150 <= image[b, a][0], 140 <= image[b, a][1], 255 >= image[b, a][1], 120 <= image[b, a][2], 220 >= image[b, a][2])):
                 print("CENTER OF BLUE BALL SHOULD BE: " + str(a) + " " + str(b))
                 cv.circle(blank, (a, b), r, (255, 255, 0), -1)
                 front.append(a)
@@ -98,57 +96,55 @@ def imageRecognitionHD(image):
 
     # Apply Gaussian blur to reduce noise
     blurred = cv.GaussianBlur(img_gray, (5, 5), 0)
+
     # Apply Canny edge detection
     edges = cv.Canny(blurred, 50, 150)
     # Find contours
     cnts, _ = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
     smallGoal = []
-    red_pixels = []
+    obstacle = []
     for contour in cnts:
         x, y, w, h = cv.boundingRect(contour)
         if image[y, x][2] > 120:
-            field = cv.drawContours(blank, [contour], -1, (150, 100, 255), 2)
+            cv.drawContours(blank, [contour], -1, (150, 100, 255), 2)
             M = cv.moments(contour)
 
             # Calculate the center of the contour
             if M["m00"] != 0:
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                if w > 500:
-                    red_pixels = field
+                if np.logical_or(w > 500, np.logical_and(50 < w, w < 100)):
                     cv.line(blank, (x, cY), (cX, cY), (0, 255, 0), 2)
                     cv.line(blank, (x + w, cY), (cX, cY), (255, 0, 0), 2)
                     cv.circle(blank, (cX, cY), 5, (150, 150, 150), -1)
                     cv.circle(blank, (x, cY), 5, (150, 150, 150), -1)
 
                     cv.circle(blank, (x, y), 10, (150, 150, 150), -1)
-                    cv.circle(blank, (x+w, y), 10, (150, 150, 150), -1)
-                    cv.circle(blank, (x, int(cY+h/2)), 10, (150, 150, 150), -1)
-                    cv.circle(blank, (x+w, int(cY+h/2)), 10, (150, 150, 150), -1)
-
-                    smallGoal.append(x)
-                    smallGoal.append(cY)
-                if w < 100:
-                    cv.circle(blank, (x, y), 10, (150, 150, 150), -1)
                     cv.circle(blank, (x + w, y), 10, (150, 150, 150), -1)
                     cv.circle(blank, (x, int(cY + h / 2)), 10, (150, 150, 150), -1)
                     cv.circle(blank, (x + w, int(cY + h / 2)), 10, (150, 150, 150), -1)
+                    if w > 500:
+                        smallGoal.append(x)
+                        smallGoal.append(cY)
 
+                    if np.logical_and(50 < w, w < 100):
+                        for points in contour:
+                            x, y = points[0]
+                            obstacle.append([x, y])
 
 
     end = time.time()
 
     time_for_transform = end - start
-
-    print("Amount of red pixels: " + str(len(red_pixels)))
+    print(obstacle)
+    print("Amount of red pixels: " + str(len(obstacle)))
     print("Amount of white pixels: " + str(white_pixels))
     print("Amount of circles: " + str(circle))
-    print("Amount of balls: "+ str(len(balls)))
+    print("Amount of balls: " + str(len(balls)))
     cv.imshow('Original', image)
     cv.imshow('Obstacles and balls drawn: ', blank)
 
     print('Time for transform: ' + str(time_for_transform))
 
     cv.waitKey(0)
-    return front, back, balls, smallGoal, red_pixels
-
+    return front, back, balls, smallGoal, obstacle
