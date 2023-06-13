3d972e41afde507aa76d1315c54d83dd51504dee
Christian H <christian@hyltoft.net>
2023-06-13 16:47:02 +0200

    boundary distance from ball and wall added

--- ball_to_the_wall.py
+++ ball_to_the_wall.py
@@ -3,18 +3,18 @@ import MoveTypes
 from Moves import MoveClass
 
 
-def move_to_ball(ball_pos, red_wall_pos):
+def move_to_ball(ball_pos, red_wall_pos, boundary_distance=10):
+    # Calculate the orthogonal line parameters
     x1, y1 = ball_pos
     x2, y2 = red_wall_pos
 
-    # Calculate the midpoint
     midpoint = ((x1 + x2) / 2, (y1 + y2) / 2)
 
-    # Calculate the slope and intercept of the orthogonal line
     if y2 - y1 == 0:
         slope = 0
     else:
         slope = -1 / ((y2 - y1) / (x2 - x1))
+
     intercept = midpoint[1] - slope * midpoint[0]
 
     # Calculate the new position on the orthogonal line
@@ -22,22 +22,20 @@ def move_to_ball(ball_pos, red_wall_pos):
     y_new = slope * x_new + intercept
 
     # Calculate the distance and angle to the new position
-    delta_x = x_new - x1
-    delta_y = y_new - y1
-    distance = math.hypot(delta_x, delta_y)
-    angle = math.atan2(delta_y, delta_x)
+    distance = math.sqrt((x_new - x1) ** 2 + (y_new - y1) ** 2)
+    angle = math.atan2(y_new - y1, x_new - x1)
 
-    # Move to the new position
-    move = MoveClass(MoveTypes.FORWARD, distance, angle)
-    move.print()
-    # Perform the movement using your existing move() function
-    result = move()  # Adjust this line based on your specific implementation
+    # Check if the distance is within the boundary
+    if distance > boundary_distance:
+        # Move to the new position
+        move = MoveClass(MoveTypes.FORWARD, distance, angle)
+        move.print()
+        # Perform the movement using your existing move() function
+        result = move()  # Adjust this line based on your specific implementation
 
     # Calculate the distance and angle to the ball
-    delta_x = x2 - x_new
-    delta_y = y2 - y_new
-    distance = math.hypot(delta_x, delta_y)
-    angle = math.atan2(delta_y, delta_x)
+    distance = math.sqrt((x2 - x_new) ** 2 + (y2 - y_new) ** 2)
+    angle = math.atan2(y2 - y_new, x2 - x_new)
 
     # Move towards the ball
     move = MoveClass(MoveTypes.FORWARD, distance, angle)
