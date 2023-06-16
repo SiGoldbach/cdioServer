4bb610065a5eb1d8640adf25bf458ae066f66fc3
siggo <sig.goldbach@gmail.com>
2023-06-16 11:04:19 +0200

    pickupBallNearWall.py refactoring

--- VariousTests/pickupBallNearWall.py
+++ VariousTests/pickupBallNearWall.py
@@ -2,11 +2,17 @@ import MoveTypes
 import Moves
 import Pathfinder
 
+DIRECTION_TOP = "top"
+DIRECTION_BOTTOM = "bottom"
+DIRECTION_LEFT = "left"
+DIRECTION_RIGHT = "right"
+DIRECTION_NOT_NEAR = "false"
+
 
 # The method to pick up ball near wall
 def move_to_wall_ball(front_pos, back_pos, ball_location, corners):
     front_pos1 = Pathfinder.robot_center_coordinates(front_pos, back_pos)
-    distance_to_ball = Pathfinder.distance_to_ball(front_pos, ball_location)
+    distance_to_ball = Pathfinder.distance_to_point(front_pos, ball_location)
     if wall_robot_align(front_pos1, back_pos, ball_location, corners):
         return Moves.MoveClass(MoveTypes.FORWARD, 500, distance_to_ball)
     else:
@@ -19,11 +25,11 @@ def wall_robot_align(front_pos, back_pos, ball_location, corners):
     direction = is_ball_near_wall(front_pos, back_pos, ball_location, corners)
     turn_align_ball = Pathfinder.calculate_turn(back_pos, front_pos, ball_location)
 
-    if direction == "left" or direction == "right":
+    if direction == DIRECTION_LEFT or direction == DIRECTION_RIGHT:
         if robot_center[1] <= ball_location[1] - 10 or robot_center[1] >= ball_location[1] + 10:
             turn_pos = (robot_center[0], ball_location[1])
             turn_align_pos = Pathfinder.calculate_turn(back_pos, front_pos, turn_pos)
-            dist_pos_align = Pathfinder.distance_to_ball(front_pos, turn_pos)
+            dist_pos_align = Pathfinder.distance_to_point(front_pos, turn_pos)
             if turn_align_pos <= 5 or turn_align_pos >= -5:
                 return Moves.MoveClass(MoveTypes.TURN, 500, turn_align_pos)
             print("i have to turn: ", turn_align_pos)
@@ -34,11 +40,11 @@ def wall_robot_align(front_pos, back_pos, ball_location, corners):
                 print("i have to turn: ", turn_align_ball)
                 return Moves.MoveClass(MoveTypes.TURN, 500, turn_align_ball)
 
-    if direction == 'top' or direction == 'down':
+    if direction == DIRECTION_TOP or direction == DIRECTION_BOTTOM:
         if robot_center[0] <= ball_location[0] - 10 or robot_center[0] >= ball_location[0] + 10:
             turn_pos = (robot_center[1], ball_location[0])
             turn_align_pos = Pathfinder.calculate_turn(back_pos, front_pos, turn_pos)
-            dist_pos_align = Pathfinder.distance_to_ball(front_pos, turn_pos)
+            dist_pos_align = Pathfinder.distance_to_point(front_pos, turn_pos)
             if turn_align_pos <= 5 or turn_align_pos >= -5:
                 print("i have to turn: ", turn_align_pos)
                 return Moves.MoveClass(MoveTypes.TURN, 500, turn_align_pos)
@@ -57,24 +63,24 @@ def is_ball_near_wall(front_pos, back_pos, ball_location, corners):
     minX, maxX, minY, maxY = Pathfinder.check_borders(corners, front_pos, back_pos)
     # Hardcoded a pixel-difference. May need change
     if ball_location[0] <= minX + 20 and ball_location[1] >= minY + 20 & ball_location[1] <= maxY - 20:
-        direction = "left"
+        direction = DIRECTION_LEFT
         print(direction)
         return direction
     if ball_location[0] >= maxX - 20 and ball_location[1] >= minY + 20 & ball_location[1] <= maxY - 20:
-        direction = "right"
+        direction = DIRECTION_RIGHT
         print(direction)
         return direction
     if ball_location[1] <= minY + 20 and minX + 20 <= ball_location[0] <= maxX - 20:
-        direction = "top"
+        direction = DIRECTION_TOP
         print(direction)
         return direction
     if ball_location[1] >= maxY - 20 and minX + 20 <= ball_location[0] <= maxX - 20:
-        direction = "down"
+        direction = DIRECTION_BOTTOM
         print(direction)
         return direction
     else:
         print("Ball is not near wall")
-        return "false"
+        return DIRECTION_NOT_NEAR
 
 
 front_pos = (200, 200)
