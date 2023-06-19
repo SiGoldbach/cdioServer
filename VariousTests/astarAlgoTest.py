12f30bfc1943e616f39f238cc1a49a77b3f8b79c
siggo <sig.goldbach@gmail.com>
2023-06-19 16:07:29 +0200

    Making astarAlgoTest.py Not do any io and put it into the test

--- VariousTests/astarAlgoTest.py
+++ VariousTests/astarAlgoTest.py
@@ -1,6 +1,9 @@
 import heapq
 import matplotlib.pyplot as plt
 
+GRID_WIDTH = 1280
+GRID_HEIGHT = 720
+
 
 # Define the heuristic function (Manhattan distance)
 def heuristic(node, goal):
@@ -73,56 +76,4 @@ def astar(start, goal, obstacles, obstacle_threshold):
     return None
 
 
-# Define the dimensions of the grid
-GRID_WIDTH = 100
-GRID_HEIGHT = 100
-
 # Define the start and goal coordinates
-start = (1, 1)
-goal = (90, 90)
-
-# Define the coordinates of the obstacles
-obstacles = [
-    (30, 40),
-    (40, 30),
-    (50, 60),
-    (60, 50),
-    (70, 20),
-    (20, 70),
-    (20, 55),
-    (55, 20),
-    (39, 39)
-]
-
-# Define the distance threshold to avoid obstacles
-obstacle_threshold = 5
-
-# Find the path using A* algorithm
-path = astar(start, goal, obstacles, obstacle_threshold)
-
-# Visualize the grid, obstacles, and path
-plt.figure(figsize=(8, 8))
-plt.xlim(0, GRID_WIDTH)
-plt.ylim(0, GRID_HEIGHT)
-plt.title('A* Algorithm - Path Planning')
-plt.xlabel('X')
-plt.ylabel('Y')
-
-# Plot the obstacles
-for obstacle in obstacles:
-    plt.scatter(obstacle[0], obstacle[1], color='red', marker='s', s=80)
-
-# Plot the path if it exists
-if path is not None:
-    x_path, y_path = zip(*path)
-    plt.plot(x_path, y_path, color='blue', linewidth=2, label='Path')
-else:
-    print("No path found.")
-
-# Plot the start and goal positions
-plt.scatter(start[0], start[1], color='green', marker='o', s=80, label='Start')
-plt.scatter(goal[0], goal[1], color='orange', marker='o', s=80, label='Goal')
-
-plt.legend()
-plt.grid(True)
-plt.show()
